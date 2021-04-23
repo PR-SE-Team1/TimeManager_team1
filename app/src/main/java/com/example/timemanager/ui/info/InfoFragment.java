85fffecf23cf6b8a7ceba4193178a20312b70fd7
nataliabachm <80454634+nataliabachm@users.noreply.github.com>
2021-04-23 16:35:19 +0200

    navigation bar
    adjusting/updating the code to match our prototype

--- app/src/main/java/com/example/timemanager/ui/info/InfoFragment.java
+++ app/src/main/java/com/example/timemanager/ui/info/InfoFragment.java
@@ -1,4 +1,4 @@
-package com.example.timemanager.ui.notifications;
+package com.example.timemanager.ui.info;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.timemanager.R;
 
-public class NotificationsFragment extends Fragment {
+public class InfoFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private InfoViewModel infoViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        infoViewModel =
+                new ViewModelProvider(this).get(InfoViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_info, container, false);
+        final TextView textView = root.findViewById(R.id.text_info);
+        infoViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
