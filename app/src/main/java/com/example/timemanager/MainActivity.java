12a4e5cec9a84814a4f83321ef22f1f153f4a17d
nataliabachm <80454634+nataliabachm@users.noreply.github.com>
2021-04-28 15:21:08 +0200

    RecyclerView and resetting some files

--- app/src/main/java/com/example/timemanager/MainActivity.java
+++ app/src/main/java/com/example/timemanager/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.util.Log;
 
 
+import com.example.timemanager.ui.projects.Project;
+import com.example.timemanager.ui.recycler.RecyclerViewAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,12 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +38,21 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        //data to populate recycle view
+        List<Project> projectList = new ArrayList<>();
+        projectList.add(new Project ("Projekt1", "kurzbeschreibung 1", 11.1, "blue"));
+        projectList.add(new Project ("Projekt2", "kurzbeschreibung 2", 12.1, "blue"));
+        projectList.add(new Project ("Projekt3", "kurzbeschreibung 3", 13.1, "blue"));
+        projectList.add(new Project ("Projekt4", "kurzbeschreibung 4", 14.1, "blue"));
+        projectList.add(new Project ("Projekt5", "kurzbeschreibung 5", 15.1, "blue"));
+        projectList.add(new Project ("Projekt6", "kurzbeschreibung 6", 16.1, "blue"));
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new RecyclerViewAdapter(projectList));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
     }
 
 
--- app/src/main/java/com/example/timemanager/ui/recycler/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/timemanager/ui/recycler/RecyclerViewAdapter.java
@@ -1,14 +1,11 @@
 package com.example.timemanager.ui.recycler;
 
-import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.timemanager.R;
@@ -18,80 +15,40 @@ import java.util.List;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
-    private List <Project> projectList;
-    private static LayoutInflater mInflater;
-    private static ItemClickListener mClickListener;
-
-    /**
-     * initialize the dataset of the Adapter
-     * @param dataSet containing data to be used by RecyclerView
-     */
-    public RecyclerViewAdapter(Context context, List<Project> dataSet){
-        this.mInflater = LayoutInflater.from(context);
-        this.projectList = dataSet;
+    private List<Project> data;
+    public RecyclerViewAdapter (List<Project> data){
+        this.data = data;
     }
 
-    /**
-     * create new views (invoked by layout manager)
-     * @param parent
-     * @param viewType
-     * @return
-     */
-    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        //create new view, which defines the UI of the list item
-        View view = mInflater.inflate(R.layout.text_row_item, parent, false);
-        return new ViewHolder(view);
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) throws NullPointerException {
+        View rowItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item_view, parent, false);
+        return new ViewHolder(rowItem);
     }
 
-    /**
-     * replace the contents of a view (invoked by layout manager)
-     * @param holder
-     * @param position
-     */
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Project project = projectList.get(position);
-        holder.textView.setText((CharSequence) project);
+    public void onBindViewHolder(RecyclerViewAdapter.ViewHolder holder, int position){
+        holder.textView.setText(position);
     }
 
     @Override
     public int getItemCount() {
-        return projectList.size();
+        return this.data.size();
     }
 
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView textView;
 
-
-    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        private final TextView textView;
-        public ViewHolder(View v) {
-            super(v);
-            textView = itemView.findViewById(R.id.projectName);
-            itemView.setOnClickListener(this);
+        public ViewHolder(View view) {
+            super(view);
+            view.setOnClickListener(this);
+            this.textView = view.findViewById(R.id.textview);
         }
 
         @Override
-        public void onClick(View v) {
-            if (mClickListener != null) mClickListener.onItemClick(v, getAdapterPosition());
+        public void onClick(View view) {
+            Toast.makeText(view.getContext(), "position : " + getLayoutPosition() + " text : " + this.textView.getText(), Toast.LENGTH_SHORT).show();
         }
-    }
-
-
-
-    // convenience method for getting data at click position
-    Project getItem(int id) {
-        return projectList.get(id);
-    }
-
-    // allows clicks events to be caught
-    void setClickListener(ItemClickListener itemClickListener) {
-        this.mClickListener = itemClickListener;
-    }
-
-    // parent activity will implement this method to respond to click events
-    public interface ItemClickListener {
-        void onItemClick(View view, int position);
     }
 
 }
