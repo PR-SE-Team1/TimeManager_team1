71d1af54450255eff0fd51bf8179d32682d4c0b4
tomWeissi <t.weissenbacher@yahoo.at>
2021-06-23 18:27:36 +0200

    Edit Projects

--- app/src/main/java/com/example/timemanager/ui/projects/ProjectActivity.java
+++ app/src/main/java/com/example/timemanager/ui/projects/ProjectActivity.java
@@ -31,6 +31,8 @@ public class ProjectActivity extends AppCompatActivity implements RecyclerViewAd
     private Button btnAddP;
     private EditText etAddP;
 
+
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
